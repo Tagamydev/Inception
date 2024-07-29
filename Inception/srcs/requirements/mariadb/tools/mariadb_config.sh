738cb0fd862c89568b128cc2e8e692c3f9bafaae
samusanc <samusanc@student.42madrid.com>
2024-07-29 22:54:39 +0200

    uploading volume db

--- Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+chmod 777 /var/run/mysqld
+
 service mariadb start
 
+echo "zone 1"
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
 	echo "Database already exists"
@@ -18,11 +23,17 @@ Y
 _EOF_
 
 
+echo "zone 2"
 echo "GRANT ALL ON *.* TO 'jefaso'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -uroot
+echo "zone 3"
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -u root
 
+echo "zone 4"
 # save data base
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+echo "zone 5"
+
+ls -la /var/lib/mysqld/
 
 fi
 
