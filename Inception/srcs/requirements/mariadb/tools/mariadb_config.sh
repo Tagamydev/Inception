fd9f796bb17eca048d43041656e21a05dcc4c95f
samusanc <samusanc@student.42madrid.com>
2024-07-29 13:31:03 +0200

    uploading something work... i guess

--- Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
-#mysqld_safe
+service mariadb start
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then 
+	echo "Database already exists"
+else
+
+mysql_secure_installation << _EOF_
+n
+n
+n
+Y
+Y
+Y
+Y
+_EOF_
+
+
+echo "GRANT ALL ON *.* TO 'jefaso'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -uroot
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -u root
+mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+service mariadb stop
+echo "finish configure the data base"
+
+exec "$@"
