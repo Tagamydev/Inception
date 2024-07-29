ab3b8fd89928c09404717eda389ed817d6b1c090
samusanc <samusanc@student.42madrid.com>
2024-07-29 14:22:19 +0200

    uploading docker

--- Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
@@ -20,7 +20,9 @@ _EOF_
 
 echo "GRANT ALL ON *.* TO 'jefaso'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -uroot
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -u root
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+# save data base
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 
