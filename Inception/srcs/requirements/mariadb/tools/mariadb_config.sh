607c544f36c3e21f25104e745ddc129ccefc7990
samusanc <samusanc@student.42madrid.com>
2024-07-30 07:14:37 +0200

    uploading wordpress sql

--- Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_config.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    mariadb_config.sh                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: samusanc <samusanc@student.42madrid.com>   +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/07/30 07:04:40 by samusanc          #+#    #+#              #
+#    Updated: 2024/07/30 07:04:40 by samusanc         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
 chmod 777 /var/run/mysqld
 
 service mariadb start
 
-echo "zone 1"
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
 	echo "Database already exists"
@@ -23,17 +34,10 @@ Y
 _EOF_
 
 
-echo "zone 2"
-echo "GRANT ALL ON *.* TO 'jefaso'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -uroot
-echo "zone 3"
+echo "GRANT ALL ON *.* TO 'jefaso'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -u root
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mariadb -u root
-
-echo "zone 4"
-# save data base
 mariadb -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-echo "zone 5"
-
-ls -la /var/lib/mysqld/
+# echo "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1');" | mariadb -u root
 
 fi
 
